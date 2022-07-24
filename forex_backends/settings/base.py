2112526a9f3ab11c9d702d2ab7a4ed81ede6821f
Deepak Patidar <developer.deepak1998@gmail.com>
2022-07-24 18:07:59 +0530

    Heroku Deployement

--- forex_backends/settings/base.py
+++ forex_backends/settings/base.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join("static")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join("media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
